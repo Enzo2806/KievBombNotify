e6f4230f28add2aa063b56d477ef2392a6d77e53
YusifCodes <malikovyusif1@gmail.com>
2023-05-31 21:26:43 +0400

    Created a routes folder, added a route for getting the districts in a separate file, fixed a minor bug in User.js model

---
+++ backend/routes/districtRoutes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const District = require("../models/District");
+
+// create a route for this endpoint /get-districts
+router.get("/get-districts", async (req,res) => {
+
+    try{
+        // this gets all districts
+        const districts = await District.find()
+        // send them back with a 200 ok status
+        res.status(200).send({"districts": districts})
+    }catch(err){
+        // send back an error if caught wioth a 400 status
+        res.status(400).send(err)
+    }
+
+})
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express();
 const mongoose = require("mongoose")
+const User = require("./models/User")
+const District = require("./models/District");
+const distRoutes = require("./routes/districtRoutes")
 
 // CONNECT TO DB
 mongoose.connect("mongodb+srv://KBNDev:KBNDevucg567@kyiv-bomb-notification.z8ok2ge.mongodb.net/?retryWrites=true&w=majority"
@@ -13,6 +16,12 @@ mongoose.connect("mongodb+srv://KBNDev:KBNDevucg567@kyiv-bomb-notification.z8ok2
 
 // Db reference
 const db = mongoose.connection
+
+// Middleware
+app.use(express.json())
+
+app.use("/", distRoutes)
+
 
 // App is lsitening on port 8080
 app.listen(8080, ()=> {console.log("Listening PORT:8080")})
