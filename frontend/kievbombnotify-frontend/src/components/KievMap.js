bbe7b8cd585116d366e6470f43ee5d3365c3fd18
Trista Lu <t09b02307@ntu.edu.tw>
2023-06-01 00:45:27 -0400

    component/KievMap: create hover-enabled map for each Kyiv's districts with svg file

--- frontend/kievbombnotify-frontend/src/components/KievMap.js
+++ frontend/kievbombnotify-frontend/src/components/KievMap.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { ReactSVG } from 'react-svg';
-import Map from '/kiev.svg';
+import { MapSVG } from './mapSVG';
+
+const handleClick = (areaName) => {
+    console.log("Clicked on:", areaName);
+};
 
 export function KievMap(props) {
     return (
         <div>
-            <div>
-                <h1>Your React Component</h1>
-                <ReactSVG src={Map} />
-            </div>
+            <MapSVG />
         </div>
     );
 }
--- frontend/kievbombnotify-frontend/src/pages/MapPage.js
+++ frontend/kievbombnotify-frontend/src/pages/MapPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./MapPage.css"
+import { KievMap } from '../components/KievMap';
 
 export function MapPage(props) {
     return (
         <div>
             <div>
                 <h1>Map</h1>
+                <KievMap />
             </div>
         </div>
     );
