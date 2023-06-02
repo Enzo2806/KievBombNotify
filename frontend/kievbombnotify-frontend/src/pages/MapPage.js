bf13d7b6ebf2510ad0b5286862c5e058762071b0
Enzo Benoit-Jeannin <enzo.benoit-jeannin@mail.mcgill.ca>
2023-06-01 23:58:25 -0400

    Map legend

--- frontend/kievbombnotify-frontend/src/pages/MapPage.js
+++ frontend/kievbombnotify-frontend/src/pages/MapPage.js
@@ -11,6 +11,20 @@ export function MapPage(props) {
                     <h1 className='title'> Live Kiev Map</h1>  
                 </div>
                 <KievMap />
+                <div className='legend'>
+                        <div className="legend-item">
+                            <div className="legend-color" style={{ backgroundColor: '#FD585E', opacity: 1 }}></div>
+                            <div className="legend-label">Severe Possibility of Attack</div>
+                        </div>
+                        <div className="legend-item">
+                            <div className="legend-color" style={{ backgroundColor: '#F4B609', opacity: 1 }}></div>
+                            <div className="legend-label">High Possibility of Attack</div>
+                        </div>
+                        <div className="legend-item">
+                            <div className="legend-color" style={{ backgroundColor: '#242424', opacity: 1 }}></div>
+                            <div className="legend-label">Low Possibility of Attack</div>
+                        </div>
+                </div>
             </div>
         </div>
     );
