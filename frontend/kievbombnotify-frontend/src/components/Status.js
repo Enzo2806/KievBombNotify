e227d8ed7680f23e224e177bd6718d143b8dd323
Trista Lu <t09b02307@ntu.edu.tw>
2023-06-02 10:02:52 -0400

    complete each block but with temporary data

--- frontend/kievbombnotify-frontend/src/components/Status.js
+++ frontend/kievbombnotify-frontend/src/components/Status.js
@@ -7,12 +7,14 @@ export function Status(props) {
     return (
         <div className="row">
             {districts.map(district => (
-                <div className="col-sm-6 report">
-                    <h4>DISTRICT: {district.value}</h4>
-                    <h4>DANGER LEVEL: {district.danger}</h4>
-                    <h4>REPORTS: {district.report}</h4>
+                <>
+                    <div className="col-sm-6 report">
+                        <h4>DISTRICT: {district.value}</h4>
+                        <h4>DANGER LEVEL:<span className={`${district.danger}`}> {district.danger}</span></h4>
+                        <h4>REPORTS: {district.report}</h4>
+                    </div>
 
-                </div>
+                </>
             ))}
         </div>
     );
--- frontend/kievbombnotify-frontend/src/pages/MapPage.js
+++ frontend/kievbombnotify-frontend/src/pages/MapPage.js
@@ -13,42 +13,46 @@ export function MapPage(props) {
 
 
     return (
-        <div style={{ display: 'flex' }}>
-            <div>
-                <div className='map-title'>
-                    <div id="circle"></div>
-                    <h1 className='title'> Live Kiev Map</h1>
-                </div>
-                <KievMap />
-                <div className='legend'>
-                    <div className="legend-item">
-                        <div className="legend-color" style={{ backgroundColor: '#FD585E', opacity: 1 }}></div>
-                        <div className="legend-label">Severe Possibility of Attack</div>
-                    </div>
-                    <div className="legend-item">
-                        <div className="legend-color" style={{ backgroundColor: '#F4B609', opacity: 1 }}></div>
-                        <div className="legend-label">High Possibility of Attack</div>
-                    </div>
-                    <div className="legend-item">
-                        <div className="legend-color" style={{ backgroundColor: '#242424', opacity: 1 }}></div>
-                        <div className="legend-label">Low Possibility of Attack</div>
-                    </div>
-                </div>
-            </div>
-            <div className="col report-container">
-                <h1 className='title'> District Status</h1>
-                <div className="district-status row d-flex">
-                    <Status />
-                </div>
-                <div>
-                    <div>
-                        <button className='report-button row' onClick={handleReportPress}>
-                            Report an Attack
-                        </button>
-                    </div>
-                </div>
-            </div>
+        <div className="container">
 
+
+            <div style={{ display: 'flex' }}>
+                <div>
+                    <div className='map-title'>
+                        <div id="circle"></div>
+                        <h1 className='title'> Live Kiev Map</h1>
+                    </div>
+                    <KievMap />
+                    <div className='legend'>
+                        <div className="legend-item">
+                            <div className="legend-color" style={{ backgroundColor: '#FD585E', opacity: 1 }}></div>
+                            <div className="legend-label">Severe Possibility of Attack</div>
+                        </div>
+                        <div className="legend-item">
+                            <div className="legend-color" style={{ backgroundColor: '#F4B609', opacity: 1 }}></div>
+                            <div className="legend-label">High Possibility of Attack</div>
+                        </div>
+                        <div className="legend-item">
+                            <div className="legend-color" style={{ backgroundColor: '#242424', opacity: 1 }}></div>
+                            <div className="legend-label">Low Possibility of Attack</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col report-container">
+                    <h1 className='title'> District Status</h1>
+                    <div className="district-status row d-flex">
+                        <Status />
+                    </div>
+                    <div>
+                        <div>
+                            <button className='report-button row' onClick={handleReportPress}>
+                                Report an Attack
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 }
