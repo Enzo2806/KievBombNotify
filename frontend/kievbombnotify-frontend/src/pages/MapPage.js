4e1170a97b223e36760f5cf4eef55bbbc2acdb09
Trista Lu <t09b02307@ntu.edu.tw>
2023-06-02 08:55:08 -0400

    install bootstrap for style

--- frontend/kievbombnotify-frontend/src/pages/MapPage.js
+++ frontend/kievbombnotify-frontend/src/pages/MapPage.js
@@ -6,43 +6,48 @@ import { useNavigate } from 'react-router-dom';
 
 export function MapPage(props) {
     const navigate = useNavigate();
-    const handleReportPress = (e) =>{
+    const handleReportPress = (e) => {
         e.preventDefault();
         navigate('/report');
     }
 
 
     return (
-        <div style={{display: 'flex'}}>
+        <div style={{ display: 'flex' }}>
             <div>
-                <div className='map-title'> 
+                <div className='map-title'>
                     <div id="circle"></div>
-                    <h1 className='title'> Live Kiev Map</h1>  
+                    <h1 className='title'> Live Kiev Map</h1>
                 </div>
                 <KievMap />
                 <div className='legend'>
-                        <div className="legend-item">
-                            <div className="legend-color" style={{ backgroundColor: '#FD585E', opacity: 1 }}></div>
-                            <div className="legend-label">Severe Possibility of Attack</div>
-                        </div>
-                        <div className="legend-item">
-                            <div className="legend-color" style={{ backgroundColor: '#F4B609', opacity: 1 }}></div>
-                            <div className="legend-label">High Possibility of Attack</div>
-                        </div>
-                        <div className="legend-item">
-                            <div className="legend-color" style={{ backgroundColor: '#242424', opacity: 1 }}></div>
-                            <div className="legend-label">Low Possibility of Attack</div>
-                        </div>
+                    <div className="legend-item">
+                        <div className="legend-color" style={{ backgroundColor: '#FD585E', opacity: 1 }}></div>
+                        <div className="legend-label">Severe Possibility of Attack</div>
+                    </div>
+                    <div className="legend-item">
+                        <div className="legend-color" style={{ backgroundColor: '#F4B609', opacity: 1 }}></div>
+                        <div className="legend-label">High Possibility of Attack</div>
+                    </div>
+                    <div className="legend-item">
+                        <div className="legend-color" style={{ backgroundColor: '#242424', opacity: 1 }}></div>
+                        <div className="legend-label">Low Possibility of Attack</div>
+                    </div>
+                </div>
+            </div>
+            <div className="col report-container">
+                <div className="district-status row">
+
+                </div>
+                <div>
+                    <div>
+                        <button className='report-button row' onClick={handleReportPress}>
+                            Report an Attack
+                        </button>
+                    </div>
                 </div>
             </div>
 
-            <div>
-                <div>
-                    <button className='report-button' onClick={handleReportPress}>
-                        Report an Attack
-                    </button>
-                </div>
-            </div>
         </div>
     );
 }
